0e3c06aa22e71b7fbef5f99b5876b50fd677831c
Evan Sosenko <razorx@evansosenko.com>
2014-05-04 00:09:46 -0700

    Added options to Unit.

--- lib/config_curator/unit.rb
+++ lib/config_curator/unit.rb
@@ -1,6 +1,24 @@
 module ConfigCurator
 
   class Unit
+
+    # Default {#options}.
+    DEFAULT_OPTIONS = {
+      # Unit installed relative to this path.
+      root: Dir.home,
+    }
+
+    def initialize options: {}
+      self.options options
+    end
+
+    # Uses {DEFAULT_OPTIONS} as initial value.
+    # @param options [Hash] merged with current options
+    # @return [Hash] current options
+    def options options = {}
+      @options ||= DEFAULT_OPTIONS
+      @options = @options.merge options
+    end
   end
 
 end
--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -1,4 +1,34 @@
 require 'spec_helper'
 
 describe ConfigCurator::Unit do
+
+  describe ".new" do
+
+    subject(:unit) { ConfigCurator::Unit.new }
+
+    it "sets default options" do
+      expect(unit.options).to eq ConfigCurator::Unit::DEFAULT_OPTIONS
+    end
+
+    it "merges default options" do
+      unit = ConfigCurator::Unit.new options: {root: '/home/user'}
+      expect(unit.options).to eq ConfigCurator::Unit::DEFAULT_OPTIONS.merge(root: '/home/user')
+    end
+  end
+
+  describe "#options" do
+
+    subject(:unit) { ConfigCurator::Unit.new }
+
+    it "merges with default options" do
+      unit.options[:root] = '/home/user'
+      expect(unit.options).to eq ConfigCurator::Unit::DEFAULT_OPTIONS.merge(root: '/home/user')
+    end
+
+    it "can be called twice and merge options" do
+      unit.options[:root] = '/home/user'
+      unit.options[:perm] = 0600
+      expect(unit.options).to eq ConfigCurator::Unit::DEFAULT_OPTIONS.merge(root: '/home/user', perm: 0600)
+    end
+  end
 end
