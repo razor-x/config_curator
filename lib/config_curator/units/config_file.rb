4289d9ead91d989ccb05b6937837b6a302bbdd6e
Evan Sosenko <razorx@evansosenko.com>
2014-05-05 14:36:18 -0700

    Filled out some of the ConfigFile class and spec.

--- lib/config_curator/units/config_file.rb
+++ lib/config_curator/units/config_file.rb
@@ -1,6 +1,47 @@
 module ConfigCurator
 
   class ConfigFile < Unit
+
+    attr_accessor :mode, :owner, :group
+
+    def destination
+      super
+      @destination ||= source
+    end
+
+    # @see Unit#install
+    def install
+      super
+      install_file
+      set_mode
+      set_owner
+    end
+
+    # @see Unit#install?
+    def install?
+      super
+      fail InstallFailed, "No file source path specified." if source_path.nil?
+      fail InstallFailed, "Source path does not exist." unless File.exists? source_path
+      true
+    end
+
+    private
+
+    # Recursively create the necessary directories and install the file.
+    def install_file
+      FileUtils.mkdir_p File.dirname(destination_path)
+      FileUtils.copy source_path, destination_path
+    end
+
+    # Set file mode.
+    def set_mode
+      FileUtils.chmod mode, destination_path unless mode.nil?
+    end
+
+    # Set file owner and group.
+    # @todo
+    def set_owner
+    end
   end
 
 end
--- spec/units/config_file_spec.rb
+++ spec/units/config_file_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
 describe ConfigCurator::ConfigFile do
+
+  subject(:config_file) { ConfigCurator::ConfigFile.new }
+
+  describe "#destination" do
+
+    it "uses the source path to set the destination" do
+      config_file.source = 'path/to/file'
+      expect(config_file.destination).to eq 'path/to/file'
+    end
+  end
+
+  describe "#install" do
+
+    it "installs the config file" do
+      allow(File).to receive(:exists?).and_return(true)
+      config_file.source = 'file'
+      config_file.destination = 'dest'
+      expect(config_file).to receive(:install_file)
+      expect(config_file).to receive(:set_mode)
+      expect(config_file).to receive(:set_owner)
+      config_file.install
+    end
+  end
+
+  describe "#install?" do
+
+    it "fails when source not given" do
+      expect { config_file.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+
+    it "fails when source does not exist" do
+      config_file.source = 'dir/that/does/not/exist'
+      expect { config_file.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+  end
 end
