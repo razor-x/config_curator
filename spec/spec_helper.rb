41c73115e1a3564c25c8e2ed9a877ae27d41b86d
Evan Sosenko <razorx@evansosenko.com>
2014-05-04 23:20:51 -0700

    Stub remove_entry_secure as a precaution.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,5 @@ SimpleCov.start
 
 RSpec.configure do |c|
   c.expect_with(:rspec) { |e| e.syntax = :expect }
+  c.before(:each) { allow(FileUtils).to receive(:remove_entry_secure).with(anything) }
 end
