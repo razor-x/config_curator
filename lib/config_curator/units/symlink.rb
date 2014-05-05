d885432954bc4ea8e65d22a48d7b5e265ff4b9b5
Evan Sosenko <razorx@evansosenko.com>
2014-05-04 22:54:26 -0700

    Filled out Symlink class and spec.

--- lib/config_curator/units/symlink.rb
+++ lib/config_curator/units/symlink.rb
@@ -1,6 +1,42 @@
 module ConfigCurator
 
   class Symlink < Unit
+
+    attr_accessor :source, :destination
+
+    # @see Unit#install
+    def install
+      super
+      symlink
+    end
+
+    # @see Unit#install?
+    def install?
+      super
+      fail InstallFailed, "No source file specified." if source_path.nil?
+      fail InstallFailed, "No destination specified." if link_path.nil?
+      fail InstallFailed, "Source file does not exist." unless File.exists? source_path
+    end
+
+    # Full path to source file.
+    # @return [String] expanded path to source file
+    def source_path
+      File.expand_path source unless source.nil?
+    end
+
+    # Full path to new symlink.
+    # @return [String] expanded path to symlink
+    def link_path
+      File.expand_path File.join(options[:root], destination) unless destination.nil?
+    end
+
+    private
+
+    # Recursively create the necessary directories and make the symlink.
+    def symlink
+      FileUtils.mkdir_p File.dirname(link_path)
+      FileUtils.symlink source_path, link_path, force: true
+    end
   end
 
 end
--- spec/units/symlink_spec.rb
+++ spec/units/symlink_spec.rb
@@ -1,4 +1,61 @@
 require 'spec_helper'
 
 describe ConfigCurator::Symlink do
+
+  subject(:symlink) { ConfigCurator::Symlink.new }
+
+  describe "#source_path" do
+
+    it "returns nil if no source given" do
+      expect(symlink.source_path).to eq nil
+    end
+
+    it "expands the path" do
+      symlink.source = 'path/../src_name'
+      expect(symlink.source_path).to eq File.expand_path('path/../src_name')
+    end
+  end
+
+  describe "#link_path" do
+
+    it "returns nil if no destination given" do
+      expect(symlink.link_path).to eq nil
+    end
+
+    it "expands the path" do
+      symlink.destination = 'path/../link_name'
+      symlink.options[:root] = '/tmp'
+      expect(symlink.link_path).to eq '/tmp/link_name'
+    end
+  end
+
+  describe "#install" do
+
+    it "makes a symbolic link" do
+      allow(File).to receive(:exists?).and_return(true)
+      symlink.source = 'file'
+      symlink.destination = 'link'
+      expect(symlink).to receive(:symlink)
+      symlink.install
+    end
+  end
+
+  describe "#install?" do
+
+    it "fails when source not given" do
+      symlink.destination = 'link_path'
+      expect { symlink.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+
+    it "fails when destination not given" do
+      symlink.source = 'file'
+      expect { symlink.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+
+    it "fails when source does not exist" do
+      symlink.destination = 'link_path'
+      symlink.source = 'path/that/does/not/exist'
+      expect { symlink.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+  end
 end
