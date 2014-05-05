592160ae9a752df269532729f97a73496513b23b
Evan Sosenko <razorx@evansosenko.com>
2014-05-04 23:42:45 -0700

    Filled out some of the Component class and spec.

--- lib/config_curator/units/component.rb
+++ lib/config_curator/units/component.rb
@@ -1,6 +1,38 @@
 module ConfigCurator
 
   class Component < Unit
+
+    # @see Unit#install
+    def install
+      super
+      install_component
+    end
+
+    # @see Unit#install?
+    def install?
+      super
+      fail InstallFailed, "No component source path specified." if source_path.nil?
+      fail InstallFailed, "No component install path specified." if destination_path.nil?
+      fail InstallFailed, "Source path does not exist." unless Dir.exists? source_path
+      true
+    end
+
+    private
+
+    # Recursively create the necessary directories and install the component.
+    # Any files in the install directory not in the source directory are removed.
+    # Use rsync if available.
+    def install_component
+
+      if command? 'rsync'
+        FileUtils.mkdir_p destination_path
+        system 'rsync', '-rt', '--del', "#{source_path}/", destination_path
+      else
+        FileUtils.remove_entry_secure destination_path
+        FileUtils.mkdir_p destination_path
+        FileUtils.cp_r "#{source_path}/.", destination_path
+      end
+    end
   end
 
 end
--- spec/units/component_spec.rb
+++ spec/units/component_spec.rb
@@ -1,4 +1,36 @@
 require 'spec_helper'
 
 describe ConfigCurator::Component do
+
+  subject(:component) { ConfigCurator::Component.new }
+
+  describe "#install" do
+
+    it "installs the component" do
+      allow(Dir).to receive(:exists?).and_return(true)
+      component.source = 'file'
+      component.destination = 'link'
+      expect(component).to receive(:install_component)
+      component.install
+    end
+  end
+
+  describe "#install?" do
+
+    it "fails when source not given" do
+      component.destination = 'inst_path'
+      expect { component.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+
+    it "fails when destination not given" do
+      component.source = 'dir'
+      expect { component.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+
+    it "fails when source does not exist" do
+      component.destination = 'inst_path'
+      component.source = 'dir/that/does/not/exist'
+      expect { component.install? }.to raise_error ConfigCurator::Symlink::InstallFailed
+    end
+  end
 end
