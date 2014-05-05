afd3bfe4b7f0a2bf62b9525944dcee7d1f16a4d9
Evan Sosenko <razorx@evansosenko.com>
2014-05-04 19:32:57 -0700

    Moved unit subclasses to units subfolder.

--- lib/config_curator.rb
+++ lib/config_curator.rb
@@ -1,11 +1,11 @@
 require 'config_curator/version'
 require 'config_curator/package_lookup'
-require 'config_curator/unit'
-require 'config_curator/file'
-require 'config_curator/symlink'
-require 'config_curator/component'
 require 'config_curator/collection'
 require 'config_curator/cli'
+require 'config_curator/unit'
+require 'config_curator/units/file'
+require 'config_curator/units/symlink'
+require 'config_curator/units/component'
 
 # Simple and intelligent configuration file management.
 module ConfigCurator
