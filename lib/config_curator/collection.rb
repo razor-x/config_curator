2ad5807990111d370a9970ce8b62031596734210
Evan Sosenko <razorx@evansosenko.com>
2014-05-05 15:34:36 -0700

    Added Collection#logger.

--- lib/config_curator/collection.rb
+++ lib/config_curator/collection.rb
@@ -1,6 +1,20 @@
 module ConfigCurator
 
   class Collection
+
+    attr_accessor :logger
+
+    def initialize logger: nil
+      self.logger = logger unless logger.nil?
+    end
+
+    # Logger instance to use.
+    # @return [Logger] logger instance
+    def logger
+      @logger ||= Logger.new($stdout).tap do |log|
+        log.progname = self.class.name
+      end
+    end
   end
 
 end
--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe ConfigCurator::Collection do
+
+  subject(:collection) { ConfigCurator::Collection.new }
+
+  describe ".new" do
+
+    it "sets the logger" do
+      logger = Logger.new(STDOUT)
+      collection = ConfigCurator::Collection.new logger: logger
+      expect(collection.logger).to be logger
+    end
+  end
+
+  describe "#logger" do
+
+    it "should make a new logger" do
+      expect(collection.logger).to be_a Logger
+    end
+  end
 end
