5a27ffd7858aff8e27bddffb86400380dde218aa
Evan Sosenko <razorx@evansosenko.com>
2014-05-07 00:43:25 -0700

    Silence spec output.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,5 +12,17 @@ SimpleCov.start
 
 RSpec.configure do |c|
   c.expect_with(:rspec) { |e| e.syntax = :expect }
+
+  stderr = $stderr
+  stdout = $stdout
+  c.before(:all) do
+    $stderr = File.open File::NULL, 'w'
+    $stdout = File.open File::NULL, 'w'
+  end
+  c.after(:all) do
+    $stderr = stderr
+    $stdout = stdout
+  end
+
   c.before(:each) { allow(FileUtils).to receive(:remove_entry_secure).with(anything) }
 end
