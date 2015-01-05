189a9118ec520226c41c3651c27b9b55c8fea780
Evan Sosenko <razorx@evansosenko.com>
2015-01-04 19:47:13 -0800

    Remove some empty lines in example spec.

--- spec/replace_cls_spec.rb
+++ spec/replace_cls_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe ReplaceGemname::ReplaceCls do
-
   subject(:replace_cls) { ReplaceGemname::ReplaceCls.new }
 
   describe ".new" do
-
     it "makes a new instance" do
       expect(replace_cls).to be_a ReplaceGemname::ReplaceCls
     end
